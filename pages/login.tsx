5f8064fa35ce8659eb4e3becf762d68690d89c21
Ammar Jussa <ammar26497@gmail.com>
2023-03-11 16:36:20 +0000

    final commit

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,47 +6,52 @@ interface Props {}
 const Login: React.FC<Props> = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  const [emailError, setEmailError] = useState<boolean>(false);
-  const [passwordError, setPasswordError] = useState<boolean>(false);
+  const [emailError, setEmailError] = useState<string>("");
+  const [passwordError, setPasswordError] = useState<string>("");
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-    if (!email.includes("@") && email.length > 2) {
-      setEmailError(true);
+    const value = e.target.value;
+    setEmail(value);
+
+    if (!value) {
+      setEmailError("Esse campo é obrigatório");
+    } else if (!/\S+@\S+\.\S{2,}/.test(value)) {
+      setEmailError("O e-mail deve ser válido");
     } else {
-      setEmailError(false);
+      setEmailError("");
     }
   };
 
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-    setPasswordError(false);
+    const value = e.target.value;
+    setPassword(value);
+    if (!value) {
+      setPasswordError("Esse campo é obrigatório");
+    } else {
+      setPasswordError("");
+    }
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!email.includes("@")) {
-      setEmailError(true);
+    if (!emailError && !passwordError) {
+      alert("success");
+    } else {
       return;
     }
-    if (password.length < 6) {
-      setPasswordError(true);
-      return;
-    }
-    // handle form submission here
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-50">
       <img src="kiwify.png" className="mx-auto h-12 w-auto" />
       <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
-        Login
+        Entrar na sua conta
       </h2>
       <p className="mt-2 text-center text-base leading-5 text-gray-600 mb-10">
-        Ou <span>&nbsp;</span>
+        Ou <></>
         <Link
           href="/signup"
-          className="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
+          className="font-medium text-indigo-500 hover:text-indigo-400 focus:outline-none focus:underline transition ease-in-out duration-150"
         >
           fazer cadastro
         </Link>
@@ -70,25 +75,23 @@ const Login: React.FC<Props> = () => {
             value={email}
             onChange={handleEmailChange}
             required
-            className="form-input block py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 w-full"
-          ></input>
-          {emailError && (
-            <p className="text-red-500 text-xs italic">
-              Please enter a valid email address.
-            </p>
-          )}
+            className={`${
+              emailError ? "border-red-500" : ""
+            } form-input block py-2 my-1 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 w-full`}
+          />
+          {emailError && <p className="text-red-500 text-xs">{emailError}</p>}
         </div>
         <div className="mb-4">
           <label
             className="block text-sm font-medium leading-5 mb-1 text-gray-500"
             htmlFor="password"
           >
-            Password
+            Senha
           </label>
           <input
             className={`${
               passwordError ? "border-red-500" : ""
-            } shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline-blue transition-colors duration-300`}
+            } form-input block py-2 my-1 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 w-full`}
             id="password"
             type="password"
             value={password}
@@ -96,25 +99,23 @@ const Login: React.FC<Props> = () => {
             required
           />
           {passwordError && (
-            <p className="text-red-500 text-xs italic">
-              Please enter a password with at least 6 characters.
-            </p>
+            <p className="text-red-500 text-xs">{passwordError}</p>
           )}
         </div>
         <div className="text-sm text-right mb-4">
           <a
             href=""
-            className="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
+            className="font-medium text-indigo-500 hover:text-indigo-400 focus:outline-none focus:underline transition ease-in-out duration-150"
           >
             Esqueceu a senha?
           </a>
         </div>
         <div className="flex justify-center">
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue transition-colors duration-300"
             type="submit"
+            className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
           >
-            Sign Up
+            Entrar
           </button>
         </div>
       </form>
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -8,54 +8,80 @@ const SignUp: React.FC<Props> = () => {
   const [repeatEmail, setRepeatEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [termsAccepted, setTermsAccepted] = useState<boolean>(false);
-  const [emailError, setEmailError] = useState<boolean>(false);
-  const [passwordError, setPasswordError] = useState<boolean>(false);
+  const [emailError, setEmailError] = useState<string>("");
+  const [emailRepeatError, setEmailRepeatError] = useState<string>("");
+  const [passwordError, setPasswordError] = useState<string>("");
+  const [termsError, setTermsError] = useState<string>("");
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-    if (!email.includes("@") && email.length > 2) {
-      setEmailError(true);
+    const value = e.target.value;
+    setEmail(value);
+
+    if (!value) {
+      setEmailError("Esse campo é obrigatório");
+    } else if (!/\S+@\S+\.\S{2,}/.test(value)) {
+      setEmailError("O e-mail deve ser válido");
     } else {
-      setEmailError(false);
+      setEmailError("");
     }
   };
 
   const handleRepeatEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
     setRepeatEmail(e.target.value);
+
+    if (!value) {
+      setEmailRepeatError("Esse campo é obrigatório");
+    } else if (!/\S+@\S+\.\S{2,}/.test(value)) {
+      setEmailRepeatError("O e-mail deve ser válido");
+    } else {
+      setEmailRepeatError("");
+    }
   };
 
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-    setPasswordError(false);
+    const value = e.target.value;
+    setPassword(value);
+    if (!value) {
+      setPasswordError("Esse campo é obrigatório");
+    } else {
+      setPasswordError("");
+    }
   };
 
   const handleTermsAcceptedChange = (
     e: React.ChangeEvent<HTMLInputElement>
   ) => {
     setTermsAccepted(e.target.checked);
+    if (!e.target.checked) {
+      setTermsError("(Esse campo é obrigatório)");
+    } else {
+      setTermsError("");
+    }
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!email.includes("@")) {
-      setEmailError(true);
+    if (email !== repeatEmail) {
+      setEmailRepeatError("Os dois e-mails devem ser iguais");
       return;
     }
-    if (password.length < 6) {
-      setPasswordError(true);
+
+    if (!emailError && !emailRepeatError && !passwordError && !termsError) {
+      alert("success");
+    } else {
       return;
     }
-    // handle form submission here
   };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <img src="kiwify.png" className="mx-auto h-12 w-auto" />
       <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">
-        Sign Up
+        Criar nova conta
       </h2>
       <p className="mt-2 text-center text-base leading-5 text-gray-600 mb-10">
-        Ou <span>&nbsp;</span>
+        Ou <></>
         <Link
           href="/login"
           className="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
@@ -82,12 +108,12 @@ const SignUp: React.FC<Props> = () => {
             value={email}
             onChange={handleEmailChange}
             required
-            className="form-input block py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 w-full"
-          ></input>
+            className={`${
+              emailError ? "border-red-500" : ""
+            } form-input block py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 w-full`}
+          />
           {emailError && (
-            <p className="text-red-500 text-xs italic">
-              Please enter a valid email address.
-            </p>
+            <p className="text-red-500 text-xs mt-1">{emailError}</p>
           )}
         </div>
         <div className="mb-4">
@@ -95,28 +121,33 @@ const SignUp: React.FC<Props> = () => {
             className="block text-sm font-medium leading-5 mb-1 text-gray-500"
             htmlFor="repeat-email"
           >
-            Repeat Email
+            Repetir e-mail
           </label>
           <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline-blue transition-colors duration-300"
+            className={` ${
+              emailRepeatError ? "border-red-500" : ""
+            } form-input block py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 w-full`}
             id="repeat-email"
             type="email"
             value={repeatEmail}
             onChange={handleRepeatEmailChange}
             required
           />
+          {emailRepeatError && (
+            <p className="text-red-500 text-xs mt-1">{emailRepeatError}</p>
+          )}
         </div>
         <div className="mb-4">
           <label
             className="block text-sm font-medium leading-5 mb-1 text-gray-500"
             htmlFor="password"
           >
-            Password
+            Senha
           </label>
           <input
             className={`${
               passwordError ? "border-red-500" : ""
-            } shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline-blue transition-colors duration-300`}
+            } form-input block py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 w-full`}
             id="password"
             type="password"
             value={password}
@@ -124,9 +155,7 @@ const SignUp: React.FC<Props> = () => {
             required
           />
           {passwordError && (
-            <p className="text-red-500 text-xs italic">
-              Please enter a password with at least 6 characters.
-            </p>
+            <p className="text-red-500 text-xs mt-1">{passwordError}</p>
           )}
         </div>
         <div className="mb-4">
@@ -174,10 +203,10 @@ const SignUp: React.FC<Props> = () => {
         </div>
         <div className="flex justify-center">
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue transition-colors duration-300"
             type="submit"
+            className="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
           >
-            Sign Up
+            Cirar conta
           </button>
         </div>
       </form>
